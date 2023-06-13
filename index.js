8219551bdb28c8352e306b5266cbaff6e258b9db
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-06-13 17:17:16 +0600

    fixed multiple time select class problem

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
-const app=express();
-const port =process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json())
 
 
-app.get('/', (req,res)=>{
-    res.send("Don't move! Dream paint is now painting you")
+app.get('/', (req, res) => {
+  res.send("Don't move! Dream paint is now painting you")
 })
 
 // mongodb 
@@ -38,48 +38,62 @@ async function run() {
 
 
     const classesCollection = client.db("dreamPaintDB").collection("classes");
-    const myClassesCollection=client.db("dreamPaintDB").collection("myClasses")
+    const myClassesCollection = client.db("dreamPaintDB").collection("myClasses")
+    const usersCollection = client.db("dreamPaintDB").collection('users')
 
-    app.get("/classes", async(req,res)=>{
-      const result= await classesCollection.find().toArray()
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray()
       res.send(result)
     })
 
 
-    app.get("/myclasses", async(req,res)=>{
-      const email=req.query.email;
+    app.get("/myclasses", async (req, res) => {
+      const email = req.query.email;
       // console.log(email);
       // if (!email) {
       //   res.send([])
       // };
-      const query={email:email}
-      const result= await myClassesCollection.find(query).toArray()
+      const query = { email: email }
+      const result = await myClassesCollection.find(query).toArray()
       res.send(result)
     })
 
-    app.get('/selected', async(req,res)=>{
+    app.get('/selected', async (req, res) => {
 
-      const result=await myClassesCollection.find().toArray()
+      const result = await myClassesCollection.find().toArray()
       res.send(result)
     })
 
-    app.post('/myclasses', async(req, res)=>{
-      const myClass =req.body;
+    app.post('/myclasses', async (req, res) => {
+      const myClass = req.body;
       // console.log(myClass)
-      // const isSelected= myClassesCollection.findOne(myClass.email)
-      
-        
-        const result=await myClassesCollection.insertOne(myClass)
-        res.send(result)
-      
+
+      const query = { email: myClass.email, name: myClass.name }
+      const isSelected = await myClassesCollection.find(query).toArray();
+
+      if (isSelected.length > 0) {
+        return res.send({ message: 'Class already selected' });
+      }
+
+
+      const result = await myClassesCollection.insertOne(myClass)
+      res.send(result)
+
     })
 
-    app.delete('/selected/:id', async(req,res)=>{
-      
-      const id=req.params.id;
+    app.delete('/selected/:id', async (req, res) => {
+
+      const id = req.params.id;
       console.log(id)
-      const query={_id: new ObjectId(id)};
-      const result=await myClassesCollection.deleteOne(query)
+      const query = { _id: new ObjectId(id) };
+      const result = await myClassesCollection.deleteOne(query)
       res.send(result)
     })
 
@@ -106,7 +120,7 @@ run().catch(console.dir);
 
 
 
-app.listen(port, ()=>{
-    console.log(`Dream Paint is painting on PORT: ${port}`);
+app.listen(port, () => {
+  console.log(`Dream Paint is painting on PORT: ${port}`);
 
 })
