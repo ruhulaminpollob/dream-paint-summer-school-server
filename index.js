cb3e3f736d6d8ff0ac0d2416b32f12a9f3dfa4db
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-06-14 10:38:53 +0600

    added jwt token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,9 +43,59 @@ async function run() {
     const usersCollection = client.db("dreamPaintDB").collection('users')
 
 
+    // JWT TOKEN  
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, env.process.SECRET_ACCESS_TOKEN, { expiresIn: '1h' })
+      res.send({token})
+    })
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
     app.post('/users', async (req, res) => {
       const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: "user already exist" })
+      }
       const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'Admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'Instructor'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    // delete users 
+
+    app.delete('/users/:id', async (req, res) => {
+
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query)
       res.send(result)
     })
 
